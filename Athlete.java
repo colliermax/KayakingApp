dacc6c0c16c007c208178da3e83e8c0b8ae92edf
colliermax <colliermax@dunstan.school.nz>
2017-05-16 13:26:15 +1200

    Start 16/5

---
+++ Athlete.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+
+/** Stores information of indivual athletes within the application
+ *
+ * @MaxCollier
+ * @16/5 v1.0
+ */
+public class Athlete
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private String nationality;
+    private double flatTime;
+    private 
+
+    /**
+     * Constructor for objects of class Athlete
+     */
+    public Athlete()
+    {
+        /**
+         * Add Parameter of how may gates there are on the course
+         */
+        ArrayList<int> penalties = new ArrayList<int>();
+       
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ KayakingApp.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/** My Assessment Application
+ * Write a description of class KayakingApp here.
+ *
+ * @MaxCollier
+ * @1.0 16/5/17
+ */
+public class KayakingApp extends JPanel {
+    
+    public KaykingApp() {
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
